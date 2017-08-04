8ca38a64f41c7f388fe361375c302d128d96c5e3
Alex Griffiths <alex@hyperion.local>
2017-08-05 08:37:38 +1000

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	Controller2D controller;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
