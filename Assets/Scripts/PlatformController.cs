234c64dea5e83a2e9b18fd48b95f03b2fe57fce5
Alex Griffiths <alex-griffiths@outlook.com>
2017-08-13 00:14:14 +1000

    More work on moving platforms. Player will now move with platforms, and can move off them. Added git ignore file

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -73,7 +73,29 @@ public class PlatformController : RaycastController {
                     }
                 }
             }
+        }
 
+        // if a passenger is on a horizontally or downward moving platform
+        if(directionY == -1 || velocity.y == 0 && velocity.x != 0)
+        {
+            float rayLength = skinWidth * 2;
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+                if (hit)
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushY = velocity.y;
+                        float pushX = velocity.x;
+
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
         }
     }
 
