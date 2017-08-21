231c984d7bd6915d08e89cd72e999a82f7096705
Alex Griffiths <alex@hyperion.local>
2017-08-21 13:19:14 +1000

    Added player sprite. Added doctor sprite. Created Doctor object, which just works as a collideable object right now. Prelim work on tiling platforms. Created new menu scene.

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour {
+	public string sceneName;
+
+	public Button button;
+	// Use this for initialization
+	void Start () {
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(ChangeLevel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ChangeLevel(){
+		Application.LoadLevel(sceneName);
+	}
+}
