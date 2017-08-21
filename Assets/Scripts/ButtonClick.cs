df95d211bb4aa0a2fae48c7792584c7bb5db109e
Alex Griffiths <alex@hyperion.local>
2017-08-21 13:51:08 +1000

    Small UI additions to menu scene

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -9,12 +9,12 @@ public class ButtonClick : MonoBehaviour {
 	public Button button;
 	// Use this for initialization
 	void Start () {
-		Button btn = button.GetComponent<Button>();
-		btn.onClick.AddListener(ChangeLevel);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(ChangeLevel);
 		
 	}
 
